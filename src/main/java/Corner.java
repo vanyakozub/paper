7589c371d2ef602135058a82bb0bf96ba5e9173f
Ivan skype <vanyakozub@mail.ru>
2020-01-10 17:21:51 +0300

    initial_commit

---
+++ src/main/java/Corner.java
@@ -0,0 +1,11 @@
+public class Corner {
+    public int x;
+    public int y;
+    public int val;
+
+    public Corner(int x, int y, int val) {
+        this.x = x;
+        this.y = y;
+        this.val = val;
+    }
+}
